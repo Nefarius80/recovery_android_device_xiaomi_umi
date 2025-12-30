be3efe0ffb03160405fb28eb84c611b5cd2173bf
Nefarius80 <alex.work.018@gmail.com>
2025-12-30 17:20:08 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 ifeq ($(TW_DEFAULT_LANGUAGE),)
-TW_DEFAULT_LANGUAGE := zh_CN
+TW_DEFAULT_LANGUAGE := RU
 endif
 TW_DEFAULT_BRIGHTNESS := 300
 TW_STATUS_ICONS_ALIGN := center
